226677dc2dc743c4a666996b0158a4a0456b06e5
eaanais <37602358+eaanais@users.noreply.github.com>
2019-03-15 12:12:09 -0300

    Soluciono problema de reset sincrono, solo resetea en posedge del clock

--- tarea1/rtl/counter_down.sv
+++ tarea1/rtl/counter_down.sv
@@ -4,7 +4,7 @@ module counter_down #(parameter N = 1)(input clk, clr, output[N-1:0] counter
 reg [N-1:0] contador;
 
 // siempre que no este activado el reset cuando hacia arriba
-  always @(posedge clk or posedge clr)
+  always @(posedge clk)
 begin
   if(clr)
   contador <= 2**N-1;
