e8740134ff0c01282b2b05e8781248a39f3ee4dd
eaanais <37602358+eaanais@users.noreply.github.com>
2019-03-15 01:10:15 -0300

    Archivo corregido, solo posee el modulo principal

--- tarea1/rtl/counter_up.sv
+++ tarea1/rtl/counter_up.sv
@@ -1,53 +1,18 @@
-// Code your design here
-module up_counter #(parameter N = 1)(input clk, clr, output[N-1:0] counter
+// contador hacia arriba de N bits
+module counter_up #(parameter N = 1)(input clk, clr, output[N-1:0] counter
     );
-reg [N-1:0] counter_up;
+reg [N-1:0] contador;
 
-// up counter
+// siempre que no este activado el reset cuando hacia arriba
   always @(posedge clk or posedge clr)
 begin
   if(clr)
- counter_up <= 0;
-else
- counter_up <= counter_up + 1;
+  contador <= 0;
+  
+  else
+  contador <= contador + 1;
 end 
 
-assign counter = counter_up;
+assign counter = contador;
 
-endmodule
-
-//testbench
-module basic_and_tb();
- 
-  reg clk, clr;
-  wire [3-1:0] counter;
- 
-  up_counter #(.N(3)) DUT (
-    .clk(clk),
-    .clr(clr),
-    .counter(counter)
-  );
-  
-  initial begin
-  $dumpfile("dump.vcd");
-  $dumpvars;
-  #200 $finish;
-  end
- 
-  
-  initial begin 
-  clk=0;
-  forever #5 clk=~clk;
-  end
-  
-  initial begin
-  clr=1;
-  #20;
-  clr=0;
-  #10;
-  clr=1;
-  #10;
-  clr=0;
-  end
- 
 endmodule
