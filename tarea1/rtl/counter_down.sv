5460cb8ed1c478c71d16f09cbf699a0df84c26ae
eaanais <37602358+eaanais@users.noreply.github.com>
2019-03-15 12:02:38 -0300

    Agrego el counter down

---
+++ tarea1/rtl/counter_down.sv
@@ -0,0 +1,18 @@
+// contador hacia abajo de N bits
+module counter_down #(parameter N = 1)(input clk, clr, output[N-1:0] counter
+    );
+reg [N-1:0] contador;
+
+// siempre que no este activado el reset cuando hacia arriba
+  always @(posedge clk or posedge clr)
+begin
+  if(clr)
+  contador <= 2**N-1;
+  
+  else
+  contador <= contador - 1;
+end 
+
+assign counter = contador;
+
+endmodule
